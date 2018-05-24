ed63a33adae442f7e126a32cbbeeeac071ad5ec8
Jungea <dmsdo2241@naver.com>
2018-05-24 21:43:20 +0900

    0517 Balance 모바일용

--- KeyMove.cs
+++ KeyMove.cs
@@ -3,26 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class KeyMove : MonoBehaviour {
-    float speed = 30.0f;
 
-    // Use this for initialization
-    void Start () {
+	// Use this for initialization
+	void Start () {
 		
 	}
-
-    // Update is called once per frame
-    void Update()
-    {
-        // 키보드 입력
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        // 이동거리 보정
-        h = h * speed * Time.deltaTime;
-        v = v * speed * Time.deltaTime;
-
-        // 회전
-        transform.rotation *= Quaternion.AngleAxis(h, Vector3.back);  // Horizontal -Z axis
-        transform.rotation *= Quaternion.AngleAxis(v, Vector3.right);  // Verticla - X axis
+	
+	// Update is called once per frame
+	void Update () {
+        var angle = Input.acceleration.y * 60;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);  // Horizontal -Z axis
     }
 }

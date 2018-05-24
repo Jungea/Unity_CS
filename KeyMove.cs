f0d538829c6bf5d594c4b265f7948e878caf7ba1
Jungea <dmsdo2241@naver.com>
2018-05-24 21:39:50 +0900

    0516 Balance 키보드조작

---
+++ KeyMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyMove : MonoBehaviour {
+    float speed = 30.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 키보드 입력
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // 이동거리 보정
+        h = h * speed * Time.deltaTime;
+        v = v * speed * Time.deltaTime;
+
+        // 회전
+        transform.rotation *= Quaternion.AngleAxis(h, Vector3.back);  // Horizontal -Z axis
+        transform.rotation *= Quaternion.AngleAxis(v, Vector3.right);  // Verticla - X axis
+    }
+}
